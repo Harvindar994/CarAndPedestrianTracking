34797352abdcc3f42eaf545f4ea00812ecdf9cbe
Harvindar Singh <41161615+Harvindar994@users.noreply.github.com>
2021-06-15 17:25:22 +0530

    Some Improvement
    Some How We have to improve data sets

--- CarAndPedestrianTracking.py
+++ CarAndPedestrianTracking.py
@@ -1,29 +1,44 @@
 import cv2
 
-imageFile = 'image.jpg'
+videoFile = 'video.mp4'
 
 # pre-trained car classifier.
 classifierFile = 'cars.xml'
+pedestrianFile = "haarcascade_fullbody.xml"
 
-# reading image file using open cv2
-image = cv2.imread(imageFile)
+# reading Video file using open cv2
+video = cv2.VideoCapture(videoFile)
 
 # creating classifier using cv2.
 CarClassifier = cv2.CascadeClassifier(classifierFile)
+PedestrianClassifier = cv2.CascadeClassifier(pedestrianFile)
 
-# converting image into gray scale.
-GrayScaleImage = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
+# here we will read video file frame by frame.
+while True:
+    readStatus, frame = video.read()
 
-# detecting cars in the given image.
-CarsCoord = CarClassifier.detectMultiScale(GrayScaleImage)
+    # if frame read successfully.
+    if readStatus:
+        # converting image into gray scale.
+        GrayScaleImage = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
 
-# drawing rectangles on the color image using Cars Coord that we created using Classifier.
-for rect in CarsCoord:
-    x, y, width, height = rect
-    cv2.rectangle(image, (x, y), (x+width, y+height), (0, 255, 0), 1)
+        # detecting cars in the given image.
+        CarsCoord = CarClassifier.detectMultiScale(GrayScaleImage)
 
-# displaying the image that we loaded.
-cv2.imshow("Cars Image", image)
-cv2.waitKey()
+        # detecting coord of pedestrians.
+        pedestrianCoord = PedestrianClassifier.detectMultiScale(GrayScaleImage)
+
+        # drawing rectangles on the color image using Cars Coord that we created using Classifier.
+        for rect in CarsCoord:
+            x, y, width, height = rect
+            cv2.rectangle(frame, (x, y), (x+width, y+height), (0, 255, 0), 1)
+
+        for rect in pedestrianCoord:
+            x, y, width, height = rect
+            cv2.rectangle(frame, (x, y), (x + width, y + height), (0, 255, 255), 1)
+
+        # displaying the image that we loaded.
+        cv2.imshow("Cars Image", frame)
+        cv2.waitKey(1)
 
 
